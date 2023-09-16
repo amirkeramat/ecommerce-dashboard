cd66246130ff3cacecfd50b5605f8a09af9a7ddf
Amirkaramat <amir_karamat@yahoo.com>
2023-09-16 12:32:50 +0330

    Create category-form.tsx

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/category-from.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { useState } from "react";
+import * as z from "zod";
+import { Size } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams, useRouter } from "next/navigation";
+
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import AlertModal from "@/components/modals/alert-modal";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z.string().min(1),
+});
+
+type SizeFormValues = z.infer<typeof formSchema>;
+
+interface SizeFormProps {
+  initialData: Size | null;
+}
+
+const SizeForm: React.FC<SizeFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit size" : "Create size";
+  const description = initialData ? "Edit a  size" : "Add a size";
+  const toastMessage = initialData ? "size updated" : "size created.";
+  const action = initialData ? "Save changes" : "Create";
+
+  const from = useForm<SizeFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  const onSubmit = async (values: SizeFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/sizes/${params.sizeId}`,
+          values
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/sizes`, values);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(
+        `/api/${params.storeId}/sizes/${params.sizeId}`
+      );
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success("Size delete successfully");
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all  products using this size first."
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+            disabled={loading}
+          >
+            <Trash className="w-4 h-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...from}>
+        <form
+          onSubmit={from.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-5 gap-8">
+            <FormField
+              control={from.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="size name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={from.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="size value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit" className="ml-auto">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
+
+export default SizeForm;
